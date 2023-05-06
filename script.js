b4fa74c065f387d28102f064d362e21cdb79c5ab
Ingthor06 <ingthor.logi@gmail.com>
2023-05-06 21:18:46 +0000

    save all

--- script.js
+++ script.js
@@ -18,10 +18,29 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style
-  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += "active";
+}
+
+// Automatic slideshow
+var slideInterval = setInterval(function() {
+  plusSlides(1);
+}, 5000);
+
+// Pause automatic slideshow on mouseover
+var slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+slideshowContainer.addEventListener('mouseenter', function() {
+  clearInterval(slideInterval);
+});
+
+// Resume automatic slideshow on mouseout
+slideshowContainer.addEventListener('mouseleave', function() {
+  slideInterval = setInterval(function() {
+    plusSlides(1);
+  }, 5000);
+});
